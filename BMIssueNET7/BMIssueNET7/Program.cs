c5208c3fa80ab0fba6dc61cb7fcb89b808642fcb
David Phillips <david.phillips@addenbrookes.nhs.uk>
2023-01-05 17:42:55 +0000

    Added solutions demonstrating Issue 486

---
+++ BMIssueNET7/BMIssueNET7/Program.cs
@@ -0,0 +1,13 @@
+using Blazored.Modal;
+using BMIssueNET7;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddBlazoredModal();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # BMIssue486
+A public respository to demonstrate Blazored Modal Issue 486
